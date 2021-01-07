f034505a1e6d10e144bc3f6af23e375491b64852
Alex Studer <alex@alexstuder.ch>
2021-01-07 14:50:02 +0100

    git init

---
+++ 1_Type.ts
@@ -0,0 +1,20 @@
+var msg:string="HelloWorld";
+console.log(msg);
+
+var nummer:number = 42 ;
+console.log(nummer);
+
+var schalter:boolean = true ;
+console.log(schalter);
+
+var irgendwas:any = "jetzt ein String" ;
+console.log(irgendwas);
+
+var irgendwas:any = 24 ;
+console.log(irgendwas);
+
+var nix:string = null ;
+console.log(nix);
+
+var nichtDefiniert:string = undefined;
+console.log(nichtDefiniert);
